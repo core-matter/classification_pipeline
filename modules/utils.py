796b7330cc380aa84010c6f57c4e4b006dcc07c9
core-matter <52578638+core-matter@users.noreply.github.com>
2021-04-23 11:06:22 +0300

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 def weights_init(m):
     """
-    :param m: Kaiming uniform weight initialization
+    Kaiming uniform weight initialization
     :return: None
     """
     if isinstance(m, nn.Conv2d):
@@ -13,6 +13,17 @@ def weights_init(m):
 
 
 def warm_up_and_annealing(it, warm_up_epochs, epochs, dataloader, resume_training):
+    """
+    linearly warms up learning rate within warm_up_epochs period
+    and anneals it afterwars using cosineLr function
+    
+    :param it:{int} batch iteration
+    :param warm_up_epochs:{int} number of epochs to warm up learning rate 
+    :param epochs: {int} total number of epochs
+    :param dataloader: dataloader
+    :param resume_training:{bool} True if training is resumed
+    :return :{float} scaling learning rate with scheduler lambdaLR
+    """
     m = warm_up_epochs * len(dataloader)
     if it < m and not resume_training:
         return it / m
@@ -22,6 +33,15 @@ def warm_up_and_annealing(it, warm_up_epochs, epochs, dataloader, resume_trainin
 
 
 def predict(model, test_loader, device):
+    """
+    predicting probs on more then one sample of data
+    used when on_sample in predict.py is not activated
+    
+    :param model: neural net
+    :param test_loader: data loader
+    :param resume_training:{str} cuda or cpu 
+    :return :{np.arrray} array with probs distribution for each sample 
+    """
     with torch.no_grad():
         logits = []
         for inputs in tqdm(test_loader):
